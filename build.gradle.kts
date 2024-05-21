ddeef9bc0bed057570fe4b47bc994af11ae7ce4f
serge loie <serge.loie@yandex.ru>
2024-05-21 17:14:20 +0500

    add necessary files

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,16 @@
 plugins {
-	java
-	id("org.springframework.boot") version "3.2.5"
-	id("io.spring.dependency-management") version "1.1.4"
+	application
+	checkstyle
+	jacoco
+	alias(libs.plugins.freefairLombokPLugin)
+	alias(libs.plugins.benManesVersionsPlugin)
+//	alias(libs.plugins.johnrengelmanShadowPlugin)
+	alias(libs.plugins.patrikerdesUseLatestVersionsPlugin)
+	alias(libs.plugins.littlerobotsVersionCatalogUpdatePlugin)
+	alias(libs.plugins.springFrameworkPlugin)
+	alias(libs.plugins.springDependencyManagementPlugin)
+	alias(libs.plugins.sonarCubePlugin)
+//	alias(libs.plugins.sentryPlugin)
 }
 
 group = "ru.anseranser"
@@ -22,15 +31,35 @@ repositories {
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-	implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	compileOnly("org.projectlombok:lombok")
-	annotationProcessor("org.projectlombok:lombok")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testImplementation("org.springframework.boot:spring-boot-testcontainers")
-	testImplementation("org.testcontainers:junit-jupiter")
-	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+	runtimeOnly(libs.h2)
+	runtimeOnly(libs.postgresql)
+
+	compileOnly(libs.lombok)
+
+	annotationProcessor(libs.lombok)
+	annotationProcessor(libs.mapstruct.annotation.processor)
+
+	testImplementation(libs.bundles.junitBundle)
+	testImplementation(platform(libs.junitBom))
+	testImplementation(libs.bundles.springTest)
+
+	implementation(libs.bundles.springUrlShortener)
+	implementation(libs.jakarta.persistence.api)
+	implementation(libs.mapstruct)
+	implementation(libs.jackson.databind.nullable)
+	implementation(libs.datafaker)
+	implementation(libs.instancio)
+	implementation(libs.postgresql)
+
+//	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+//	implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
+//	implementation("org.springframework.boot:spring-boot-starter-web")
+//	compileOnly("org.projectlombok:lombok")
+//	annotationProcessor("org.projectlombok:lombok")
+//	testImplementation("org.springframework.boot:spring-boot-starter-test")
+//	testImplementation("org.springframework.boot:spring-boot-testcontainers")
+//	testImplementation("org.testcontainers:junit-jupiter")
+//	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 tasks.withType<Test> {
