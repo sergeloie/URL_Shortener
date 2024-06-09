a20c558a3bcfb3a82d8845a244cd33309143b22b
daBoss <serge.loie@yandex.ru>
2024-06-09 13:48:38 +0500

    add mapper Tests

--- src/main/java/ru/anseranser/urlshortener/UrlShortenerApplication.java
+++ src/main/java/ru/anseranser/urlshortener/UrlShortenerApplication.java
@@ -2,6 +2,8 @@ package ru.anseranser.urlshortener;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import ru.anseranser.urlshortener.utils.RandomStringGenerator;
 
 @SpringBootApplication
 public class UrlShortenerApplication {
@@ -9,6 +11,11 @@ public class UrlShortenerApplication {
     public static void main(String[] args) {
 
         SpringApplication.run(UrlShortenerApplication.class, args);
+    }
+
+    @Bean
+    public RandomStringGenerator getRandomStringGenerator() {
+        return new RandomStringGenerator();
     }
 
 }
--- src/test/java/ru/anseranser/urlshortener/UrlShortenerApplicationTests.java
+++ src/test/java/ru/anseranser/urlshortener/UrlShortenerApplicationTests.java
@@ -1,13 +1,28 @@
 package ru.anseranser.urlshortener;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import ru.anseranser.urlshortener.dto.link.LinkDto;
+import ru.anseranser.urlshortener.mapper.LinkMapper;
+import ru.anseranser.urlshortener.model.Link;
 
 @SpringBootTest
 class UrlShortenerApplicationTests {
 
+	@Autowired
+	private LinkMapper linkMapper;
+
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void mapperTest() {
+		Link link = new Link();
+		link.setShortLink("12345678");
+		LinkDto linkDTO = linkMapper.toDto(link);
+		System.out.println(linkDTO.getShortLink());
 	}
 
 }
