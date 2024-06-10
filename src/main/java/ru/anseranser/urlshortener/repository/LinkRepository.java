c00c9ad5d0d9e229acecef50f216b20360c9c66a
serge loie <serge.loie@yandex.ru>
2024-06-10 14:21:59 +0500

    add to LinkRepository method findByShortLink()

--- src/main/java/ru/anseranser/urlshortener/repository/LinkRepository.java
+++ src/main/java/ru/anseranser/urlshortener/repository/LinkRepository.java
@@ -4,6 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.lang.NonNull;
 import ru.anseranser.urlshortener.model.Link;
 
+import java.util.Optional;
+
 public interface LinkRepository extends JpaRepository<Link, Long> {
     boolean existsByShortLink(@NonNull String shortLink);
+    Optional<Link> findByShortLink(String shortLink);
 }
